de6fce92ad9e31cc090a7613124607141a8d2abf
raydenm <raydenm928@gmail.com>
2024-08-06 17:11:54 +0800

    feat: change style

--- src/components/stack-list.js
+++ src/components/stack-list.js
@@ -2,10 +2,10 @@ import { STACK_LIST } from '@/lib/constants'
 
 export const StackList = () => {
   return (
-    <div className="flex flex-wrap gap-6">
+    <div className="grid grid-cols-3 gap-6 sm:flex sm:flex-wrap">
       {STACK_LIST.map(({ title, icon, url }, key) => (
         <a key={key} href={`${url}?ref=${process.env.WEBSITE_URL}`} target="_blank" rel="noopener noreferrer">
-          <div className="group flex size-[108px] cursor-pointer flex-col items-center justify-center rounded-lg shadow-md transition duration-300 hover:-translate-y-1 hover:bg-sky-100 hover:shadow-lg">
+          <div className="group flex h-[100px] cursor-pointer flex-col items-center justify-center rounded-lg shadow-md transition duration-300 hover:-translate-y-1 hover:bg-sky-100 hover:shadow-lg sm:size-[108px]">
             <img src={icon.src} alt={title} className="aspect-square size-10 rounded border-none" />
             <div className="mt-3 text-sm text-gray-700 group-hover:text-gray-950">{title}</div>
           </div>
