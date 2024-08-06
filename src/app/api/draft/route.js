a4b586b78b8b60c313e92dec153bdecdeb246bc0
Guanghao <Guanghao@123.com>
2024-08-06 10:53:25 +0800

    feat: add viewcount

--- src/app/api/draft/route.js
+++ src/app/api/draft/route.js
@@ -6,7 +6,6 @@ export async function GET(request) {
   const searchParams = request.nextUrl.searchParams
   const secret = searchParams.get('secret')
 
-  // http://localhost:3000/api/draft?secret=nMLod-8aC9fEhQkZ-4XMokIlLJD_96t3G41lzaIaMKM
   if (secret !== process.env.CONTENTFUL_PREVIEW_SECRET) {
     return NextResponse.json({ error: 'Invalid token' }, { status: 401 })
   }
